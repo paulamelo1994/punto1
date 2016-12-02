43d460d1f023ce68aea412c7d95f63eec2547da1
PAULA DANIELA MELO ANGULO <pauladma@s3pc19.eisc.univalle.edu.co>
2016-12-02 15:22:12 -0500

    clases2

---
+++ llaves.cpp
@@ -0,0 +1,5 @@
+#include "llaves.h"
+
+ClaveLlaves::ClaveLlaves():public Clave{
+
+}
---
+++ llaves.h
@@ -0,0 +1,20 @@
+#ifndef LLAVES_H
+#define LLAVES_H
+
+#include <string>
+using namespace std;
+
+
+class ClaveLlaves {
+
+		private:
+
+
+		public:
+			ClaveLlaves();
+			~ClaveLlaves();
+			string textoClave()= "";
+
+};
+
+#endif
