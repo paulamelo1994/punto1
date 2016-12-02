c6b9b0f9bad2d67be098df7aacdcc0e303e2d8aa
PAULA DANIELA MELO ANGULO <pauladma@s3pc19.eisc.univalle.edu.co>
2016-12-02 14:47:43 -0500

    hola amigos

---
+++ clave.h
@@ -0,0 +1,20 @@
+#ifndef CLAVE_H
+#define CLAVE_H
+
+#include <string>
+using namespace std;
+
+
+class Clave {
+
+		protected:
+
+
+		public:
+			Clave();
+			~Clave();
+			virtual string textoClave();
+
+};
+
+#endif
